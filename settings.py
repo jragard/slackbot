0192e2d94896c4e7d95fdc0ab58ab804c6b7a4ef
Ryan <jragard@gmail.com>
2018-09-18 22:09:28 -0400

    added logging and signal handling, added virtual environment to .gitignore file

---
+++ settings.py
@@ -0,0 +1,38 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+import logging
+
+dotenv_path = join(dirname(__file__), 'commands.env')
+print 'dotenv path', dotenv_path
+load_dotenv('./commands.env')
+
+SLACK_BOT_TOKEN = os.getenv('SLACK_BOT_TOKEN')
+
+# Sets up logger basic config
+# logging.basicConfig(level=logging.DEBUG, filename='slackbot.log', format='%(levelname)s:%(name)s:%(message)s')
+
+# sets up logger
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+
+formatter = logging.Formatter('%(levelname)s:%(name)s:%(message)s')
+
+file_handler = logging.FileHandler('slackbot.log')
+file_handler.setFormatter(formatter)
+
+stream_handler = logging.StreamHandler()
+
+logger.addHandler(file_handler)
+logger.addHandler(stream_handler)
+
+log_object = open('./slackbot.log', 'r')
+log_text = log_object.read()
+print 'log length', len(log_text)
+
+if len(log_text) > 15000:
+    log = open('./slackbot.log', 'w')
+    log.write("""Log File too long, overwriting
+                --------------------------------
+              """)
--- starterbot.py
+++ starterbot.py
@@ -1,12 +1,15 @@
-import os
 import time
 import re
+import settings
+import signal
 from slackclient import SlackClient
 
-
 # instantiate Slack client
-slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
+slack_client = SlackClient(settings.SLACK_BOT_TOKEN)
+
+
 # starterbot's user ID in Slack: value is assigned after the bot starts up
+
 starterbot_id = None
 
 # constants
@@ -62,7 +65,25 @@ def handle_command(command, channel):
         response = res + " " + res + " " + res
     if command == "exit":
         response = "See ya later!"
+        settings.logger.info(command)
+        settings.logger.info('Starterbot is Disconnecting, uptime = {} seconds'.format(
+            time.time() - start_time
+        ))
         exit_flag = False
+    if command == "ping":
+        response = "ryanbot is active, uptime = {} seconds".format(
+            time.time() - start_time)
+        settings.logger.info(command)
+        settings.logger.info(response)
+    if command == "help":
+        response = """Here is a list of commands:
+        exit
+        ping
+        help
+        do
+                   """
+        settings.logger.info(command)
+        settings.logger.info(response)
 
     # Sends the response back to the channel
     slack_client.api_call(
@@ -71,28 +92,50 @@ def handle_command(command, channel):
         text=response or default_response
     )
 
+def signal_handler(sig_num, frame):
+    global exit_flag
+    """
+    This is a handler for SIGTERM and SIGINT. Other signals can be mapped here
+    as well (SIGHUP?) Basically it just sets a global flag, and main() will
+    exit it's loop if the signal is trapped.
+    :param sig_num: The integer signal number that was trapped from the OS.
+    :param frame: Not used
+    :return None
+    """
+    print 'Signal Number ', sig_num
+    signame = dict((k, v) for v, k in reversed(sorted(signal.__dict__.items()))
+                   if v.startswith('SIG') and not v.startswith('SIG_'))
+    settings.logger.debug('Received {}, Disconnecting, uptime = {} seconds'.format(signame[sig_num], time.time() - start_time))
+    exit_flag = False
+
 
 if __name__ == "__main__":
-    # global exit_flag
+    start_time = time.time()
+
+    signal.signal(signal.SIGINT, signal_handler)
+    signal.signal(signal.SIGTERM, signal_handler)
+
     if slack_client.rtm_connect(with_team_state=False):
         print("Starter Bot connected and running!")
-
+        settings.logger.info('StarterBot connected')
+        
         # Read bot's user ID by calling Web API method `auth.test`
         starterbot_id = slack_client.api_call("auth.test")["user_id"]
 
         # Send a message to a channel announcing bot is online
         connect_message = "I am online!"
 
-        slack_client.api_call(
-            "chat.postMessage",
-            channel="CCQ1869JA",
-            text=connect_message
-        )
+        # slack_client.api_call(
+        #     "chat.postMessage",
+        #     channel="CCD7USCR0",
+        #     text=connect_message
+        # )
 
         while exit_flag is True:
             command, channel = parse_bot_commands(slack_client.rtm_read())
             if command:
                 handle_command(command, channel)
             time.sleep(RTM_READ_DELAY)
+        
     else:
         print("Connection failed. Exception traceback printed above")
